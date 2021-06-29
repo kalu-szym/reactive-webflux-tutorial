2145e8b2900f78a38d58692aac89c72b2d47bb09
ksz <kalu.szym@gmail.com>
2021-06-29 11:19:33 +0100

    refactoring using interfaces instead of implementations

--- reactive-webflux-tutorial/src/main/java/com/ourcastle/reactivewebfluxtutorial/controller/ProductController.java
+++ reactive-webflux-tutorial/src/main/java/com/ourcastle/reactivewebfluxtutorial/controller/ProductController.java
@@ -1,54 +1,44 @@
 package com.ourcastle.reactivewebfluxtutorial.controller;
 
-import com.ourcastle.reactivewebfluxtutorial.dao.ProductDaoImp;
+import com.ourcastle.reactivewebfluxtutorial.dao.ProductDao;
 import com.ourcastle.reactivewebfluxtutorial.model.Product;
-import com.ourcastle.reactivewebfluxtutorial.service.ProductServiceImp;
+import com.ourcastle.reactivewebfluxtutorial.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.Comparator;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-
-    private final ProductServiceImp productServiceImp;
-
-    private final ProductDaoImp productDaoImp;
+    @Autowired
+    private ProductService productService;
 
     @Autowired
-    public ProductController(ProductDaoImp productDaoImp, ProductServiceImp productServiceImp) {
-        this.productDaoImp = productDaoImp;
-        this.productServiceImp = productServiceImp;
-    }
+    private ProductDao productDao;
 
     @GetMapping("/{name}")
     private Mono<Product> getProductByName(@PathVariable String name) {
-        return productDaoImp.getProductByName(name);
+        return productDao.getProductByName(name);
     }
-
 
     //sort by name descending - modify inbetween (service layer) and test it
     @GetMapping
     private Flux<Product> getAllProducts_SortedByNameDescendingOrder() {
-        return productServiceImp.getAllProducts_SortedByNameDescendingOrder();
+        return productService.getAllProducts_SortedByNameDescendingOrder();
     }
 
     @PostMapping(path = "/save", consumes = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<Void> saveProduct(@RequestBody Product product) {
-        return productDaoImp.saveProduct(product);
+        return productDao.saveProduct(product);
     }
 
     @DeleteMapping("/{name}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public Mono<Void> deleteProduct(@PathVariable String name) {
-        return productDaoImp.deleteProduct(name);
+        return productDao.deleteProduct(name);
     }
 }
--- reactive-webflux-tutorial/src/main/java/com/ourcastle/reactivewebfluxtutorial/service/ProductServiceImp.java
+++ reactive-webflux-tutorial/src/main/java/com/ourcastle/reactivewebfluxtutorial/service/ProductServiceImp.java
@@ -1,6 +1,6 @@
 package com.ourcastle.reactivewebfluxtutorial.service;
 
-import com.ourcastle.reactivewebfluxtutorial.dao.ProductDaoImp;
+import com.ourcastle.reactivewebfluxtutorial.dao.ProductDao;
 import com.ourcastle.reactivewebfluxtutorial.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,17 +11,13 @@ import java.util.Comparator;
 @Service
 public class ProductServiceImp implements ProductService{
 
-    private final ProductDaoImp productDaoImp;
-
     @Autowired
-    public ProductServiceImp(ProductDaoImp productDaoImp) {
-        this.productDaoImp = productDaoImp;
-    }
+    private ProductDao productDao;
 
     @Override
     public Flux<Product> getAllProducts_SortedByNameDescendingOrder() {
         Comparator<Product> sortProductByNameDescendingOrder = Comparator.comparing(Product::getName, (p1, p2) -> {return p2.compareTo(p1);});
-        return productDaoImp.getAllProducts().sort(sortProductByNameDescendingOrder);
+        return productDao.getAllProducts().sort(sortProductByNameDescendingOrder);
     }
 
 }
