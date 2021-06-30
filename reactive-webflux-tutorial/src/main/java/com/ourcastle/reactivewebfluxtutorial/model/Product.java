16f1747360b7ff941327cdad2bd9e7b66e9d6960
ksz <kalu.szym@gmail.com>
2021-06-30 15:07:04 +0100

    Refactoring - added lombok to Product.class

--- reactive-webflux-tutorial/src/main/java/com/ourcastle/reactivewebfluxtutorial/model/Product.java
+++ reactive-webflux-tutorial/src/main/java/com/ourcastle/reactivewebfluxtutorial/model/Product.java
@@ -1,11 +1,16 @@
 package com.ourcastle.reactivewebfluxtutorial.model;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.stereotype.Component;
 
 import java.util.Comparator;
-import java.util.Objects;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
 @Component
 public class Product {
 
@@ -14,64 +19,11 @@ public class Product {
     @Id
     private int id;
 
+    @NonNull
     private String name;
 
+    @NonNull
     private int numberOfProducts;
-
-    public Product() {
-    }
-
-    public Product(String name) {
-        this.name = name;
-    }
-
-    public Product(String name, int numberOfProducts) {
-        this.name = name;
-        this.numberOfProducts = numberOfProducts;
-    }
-
-    public Product(int id, String name, int numberOfProducts) {
-        this.id = id;
-        this.name = name;
-        this.numberOfProducts = numberOfProducts;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getNumberOfProducts() {
-        return numberOfProducts;
-    }
-
-    public void setNumberOfProducts(int numberOfProducts) {
-        this.numberOfProducts = numberOfProducts;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Product product = (Product) o;
-        return id == product.id && numberOfProducts == product.numberOfProducts && name.equals(product.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, numberOfProducts);
-    }
 
     //key extractor
     private static final Comparator<Product> sortProductByNameDescendingOrder = Comparator.comparing(Product::getName).reversed();
